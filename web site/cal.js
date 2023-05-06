ea0174e38ca4d563a7661156e2c8f7c41a75bb1c
ppgheshan <110448800+ppgheshan@users.noreply.github.com>
2023-05-06 13:30:36 +0530

    sample website

---
+++ web site/cal.js
@@ -0,0 +1,6 @@
+function calculate()
+{
+       var num1 = parseInt(document.getElementById("firstNumber").value);
+       var num2 = parseInt(document.getElementById("secondNumber").value);
+        document.getElementById("result").innerHTML = num1 * num2;
+}
